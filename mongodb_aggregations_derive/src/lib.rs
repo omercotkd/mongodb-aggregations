4570d3bf5e5d35d010e3bb36ddad3d57b71e1857
Omer Cohen <omercotkd@gmail.com>
2023-11-30 00:31:23 +0200

    will keep cfg attr on fields

--- mongodb_aggregations_derive/src/lib.rs
+++ mongodb_aggregations_derive/src/lib.rs
@@ -87,28 +87,57 @@ fn impl_into_document(ast: &syn::DeriveInput, stage_name: &String) -> TokenStrea
         syn::Data::Union(_) => panic!("Unions are not supported"),
     }
     .iter()
-    .map(|field| (field.ident.as_ref().unwrap(), &field.ty))
+    .map(|field| {
+        (
+            field.ident.as_ref().unwrap(),
+            &field.ty,
+            field
+                .attrs
+                .iter()
+                .filter(|attr| attr.path().is_ident("cfg"))
+                .collect::<Vec<_>>(),
+        )
+    })
     .collect::<Vec<_>>();
 
     let mut document_fields = Vec::new();
-
-    for (field_name, _field_type) in fields {
+    
+    for (field_name, field_type, field_attrs) in fields {
         let key = field_name.to_string().to_camal_case();
 
-        document_fields.push(quote! {
+        let normal_quote = quote! {
+            #(#field_attrs)*
             inner_document.insert(#key, self.#field_name);
-        });
+        };
+
+        match field_type {
+            syn::Type::Path(ref type_path) => {
+                let type_name = type_path.path.segments.last().unwrap().ident.to_string();
+
+                if type_name == "Option" {
+                    document_fields.push(quote! {
+                        #(#field_attrs)*
+                        if let Some(value) = self.#field_name {
+                            inner_document.insert(#key, value);
+                        }
+                    });
+                } else {
+                    document_fields.push(normal_quote)
+                }
+            }
+            _ => document_fields.push(normal_quote),
+        }
     }
 
     quote! {
-        impl Into<bson::Document> for #name {
-            fn into(self) -> bson::Document {
+        impl Into<::bson::Document> for #name {
+            fn into(self) -> ::bson::Document {
 
-                let mut inner_document = bson::Document::new();
+                let mut inner_document = ::bson::Document::new();
 
                 #(#document_fields)*
 
-                let mut document = bson::Document::new();
+                let mut document = ::bson::Document::new();
 
                 document.insert(#stage_name, inner_document);
 
@@ -130,7 +159,7 @@ impl ToCamalCase for String {
 
         for c in self.chars() {
             match c {
-                '_' | ' ' => make_upper = true,
+                '_' => make_upper = true,
                 _ if first => {
                     result.push(c.to_ascii_lowercase());
                     first = false;
